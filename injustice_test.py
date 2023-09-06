7c49b8de2ddd0edb919f759358bf52b0c4769394
Daniel Wang <danielwang9@protonmail.com>
2023-09-06 18:19:57 -0500

    Uncomment account manager init & update injusticejudge

--- injustice_test.py
+++ injustice_test.py
@@ -9,8 +9,8 @@ async def test_injustice():
     # this took me so long to debug... I tried to make my own AccountManager, while
     # `utilities.py` was using the one from `global_stuff`... UGH.
     # uncomment the two lines below if the games are not cached yet
-    # from global_stuff import account_manager
-    # await account_manager.connect_and_login()
+    from global_stuff import account_manager
+    await account_manager.connect_and_login()
     print("===============================")
     for reason, link in links.items():
         print("reason: ", reason, "\n-----------")
