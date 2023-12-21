3f0c9619f7a2880c190ef9098f9523ed04b1bf5b
Daniel Wang <danielwang9@protonmail.com>
2023-12-20 20:22:27 -0600

    Update InjusticeJudge

--- unit_test_imports.py
+++ unit_test_imports.py
@@ -35,4 +35,5 @@ links = {
     "tenhou sanma": "https://tenhou.net/4/?log=2023081610gm-0099-0000-6e203a73&tw=2",
     "tenhou double ron pao with honba": "https://tenhou.net/3/?log=2016090115gm-0089-0000-8d300f3c&tw=0&ts=1",
     "shousuushi denied": "https://mahjongsoul.game.yo-star.com/?paipu=230814-90607dc4-3bfd-4241-a1dc-2c639b630db3_a878761203",
+    "deal-in tile was last discard": "https://tenhou.net/0/?log=2023121410gm-0089-0000-f96301cf&tw=1",
 }
